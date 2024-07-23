91eff9a44f765b8aad0f6b4913c01c3b5c4bc9a2
Stefan van Herwijnen <stefan@devliert.info>
2024-07-23 13:52:55 +0200

    feat(InvoiceStatusToggle): collapse toggle into a dropdown on small screens

--- packages/app/src/components/invoice/InvoiceStatusToggle.vue
+++ packages/app/src/components/invoice/InvoiceStatusToggle.vue
@@ -1,11 +1,32 @@
 <template>
   <q-btn-toggle
+    v-if="$q.screen.gt.md"
     :model-value="modelValue"
     push
     toggle-color="primary"
     :options="options"
     @update:model-value="($event) => emit('update:modelValue', $event)"
   />
+  <q-btn-dropdown
+    v-else
+    :label="
+      options.find((option) => option.value === modelValue)?.label ||
+      lang.invoice.fields.status
+    "
+  >
+    <q-list>
+      <q-item
+        v-for="option in options"
+        :key="option.value"
+        clickable
+        @click="emit('update:modelValue', option.value)"
+      >
+        <q-item-section>
+          <q-item-label>{{ option.label }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-btn-dropdown>
 </template>
 
 <script setup lang="ts">
@@ -13,6 +34,7 @@ import { QBtnToggle } from 'quasar'
 import { useLang } from '../../lang/index.js'
 import { computed } from 'vue'
 import { InvoiceStatus } from '@slimfact/api/zod'
+import { useQuasar } from 'quasar'
 
 export interface Props {
   modelValue: string | null
@@ -20,6 +42,7 @@ export interface Props {
 defineProps<Props>()
 const emit = defineEmits<{ (e: 'update:modelValue', val: string): void }>()
 
+const $q = useQuasar()
 const lang = useLang()
 const options = computed(() => [
   {
