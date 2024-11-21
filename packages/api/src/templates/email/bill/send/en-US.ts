21453f6c81df7e02b9cdb31dbe40b973da36dcf5
Stefan van Herwijnen <stefan@devliert.info>
2024-11-21 14:16:50 +0100

    chore: change send bill email

--- packages/api/src/templates/email/bill/send/en-US.ts
+++ packages/api/src/templates/email/bill/send/en-US.ts
@@ -1,11 +1,12 @@
 import { c } from 'compress-tag'
 
-const subject = `Outstanding bill`
+const subject = `You have an outstanding bill`
 const body = c`</p>Dear {{#if clientDetails.contactPersonName}}{{clientDetails.contactPersonName}}{{else}}{{clientDetails.companyName}}{{/if}},</p>
 
-<p>In the attachment you can find the bill with an amount due of {{amountDue}}.</p>
+<p>In the attachment you can find the bill with an amount due of {{amountDue}}.
+We would like to request you to pay this amount online.</p>
 
-<p>You can also view and pay the bill <a href="\\{{invoiceUrl}}">here</a>.
+<p>You can view and pay the bill <a href="\\{{invoiceUrl}}">here</a>.
 
 <p>If you have any questions about this bill, you can contact us.</p>
 
--- packages/api/src/templates/email/bill/send/nl.ts
+++ packages/api/src/templates/email/bill/send/nl.ts
@@ -1,12 +1,13 @@
 import { c } from 'compress-tag'
 
-const subject = `Openstaande rekening`
+const subject = `U heeft een openstaande rekening`
 const body = c`
   <p>Beste {{#if clientDetails.contactPersonName}}{{clientDetails.contactPersonName}}{{else}}{{clientDetails.companyName}}{{/if}},</p>
 
-  <p>Bijgevoegd treft u uw rekening aan met een openstaand bedrag ter hoogte van {{amountDue}}.</p>
+  <p>Bijgevoegd treft u uw rekening aan met een openstaand bedrag ter hoogte van {{amountDue}}.
+  We willen u verzoeken om dit bedrag online te betalen.</p>
 
-  <p>U kunt de rekening ook <a href="\\{{invoiceUrl}}">hier</a> bekijken en betalen.
+  <p>U kunt de rekening <a href="\\{{invoiceUrl}}">hier</a> bekijken en betalen.
 
   <p>Mocht u nog vragen hebben over deze rekening, dan kunt u contact met ons opnemen.</p>
 
