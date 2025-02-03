9937cde7c1c5196ef990f0a9fce84ea902fc7122
Stefan van Herwijnen <stefan@devliert.info>
2025-02-03 12:19:53 +0100

    chore: update send bill templates

--- packages/api/src/templates/email/bill/send/en-US.ts
+++ packages/api/src/templates/email/bill/send/en-US.ts
@@ -4,7 +4,7 @@ const subject = `View or pay your outstanding bill`
 const body = c`</p>Dear {{#if clientDetails.contactPersonName}}{{clientDetails.contactPersonName}}{{else}}{{clientDetails.companyName}}{{/if}},</p>
 
 <p>In the attachment you can find the bill with an amount due of {{amountDue}}.
-We would like to request you to pay this amount with the link below.</p>
+We would like to request you to pay this amount <b>with the link below</b>.</p>
 
 <p>You can view and pay the bill <a href="\\{{invoiceUrl}}">here</a>.
 
