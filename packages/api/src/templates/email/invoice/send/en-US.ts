a91b532317260e84dc6057a7b5899987e61fb023
Stefan van Herwijnen <stefan@devliert.info>
2025-04-16 11:40:50 +0200

    feat(api): update sendInvoice template

--- packages/api/src/templates/email/invoice/send/en-US.ts
+++ packages/api/src/templates/email/invoice/send/en-US.ts
@@ -8,7 +8,7 @@ const body = c`
   {{#unless paid}}
   <p>We would like to ask you to pay the amount of {{totalIncludingTax}} within {{paymentTermDays}} days (before \\{{dueDate}}) on account number {{companyDetails.iban}} in the name of {{companyDetails.name}} with mention of the invoice number.</p>
   {{/unless}}
-  <p>You can also view the invoice <a href="\\{{invoiceUrl}}">here</a>.
+  <p>You can also view and pay the invoice <a href="\\{{invoiceUrl}}">here</a>.
 
   <p>If you have any questions about this invoice, you can contact us.</p>
 
--- packages/api/src/templates/email/invoice/send/nl.ts
+++ packages/api/src/templates/email/invoice/send/nl.ts
@@ -7,7 +7,7 @@ const body = c`<p>Beste {{#if clientDetails.contactPersonName}}{{clientDetails.c
   {{#unless paid}}
   <p>Gelieve het verschuldigde bedrag ter hoogte van {{totalIncludingTax}} te betalen binnen {{paymentTermDays}} dagen (voor \\{{dueDate}}) op rekeningnummer {{companyDetails.iban}} t.n.v. {{companyDetails.name}} onder vermelding van factuurnummer.</p>
   {{/unless}}
-  <p>U kunt de factuur ook <a href="\\{{invoiceUrl}}">hier</a> bekijken.
+  <p>U kunt de factuur ook <a href="\\{{invoiceUrl}}">hier</a> bekijken en betalen.
 
   <p>Mocht u nog vragen hebben over deze factuur, dan kunt u contact met ons opnemen.</p>
 
